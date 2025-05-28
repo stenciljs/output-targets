5dbaba970d753c46418bc7615f24ca070ca513d7
Christian Bromann <git@bromann.dev>
2025-05-28 14:37:30 -0700

    uncomment tests

--- example-project/next-app/test/test.e2e.ts
+++ example-project/next-app/test/test.e2e.ts
@@ -4,20 +4,6 @@ describe('Next.js 14 React 18 SSR Integration', () => {
   runTestScenarios({
     exclude: [
       /**
-       * minor differences with how properties are rendered compared to Next.js v15, e.g.
-       *
-       * ```patch
-       * +     baz="[object Map]"
-       *       class="hydrated sc-my-complex-props-h"
-       * -     grault="Infinity"
-       * +     foo="[object Object]"
-       * +     grault="Infinity"
-       * +     quux="[object Set]"
-       * ```
-       */
-      'complex-props-scoped',
-      'complex-props-shadow',
-      /**
        * currently broken in Next.js
        *
        * Problem here is that React doesn't re-render the component at runtime to initate the event handlers.
