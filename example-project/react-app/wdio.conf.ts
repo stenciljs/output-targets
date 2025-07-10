a3d9b3fd2b8ad320babf32c58d6b4d72ce45e862
Christian Bromann <git@bromann.dev>
2025-07-10 10:41:36 -0700

    make test more stable

--- example-project/react-app/wdio.conf.ts
+++ example-project/react-app/wdio.conf.ts
@@ -43,6 +43,7 @@ export const config: WebdriverIO.Config = {
   // from the same test should run tests.
   //
   maxInstances: 10,
+  specFileRetries: 2,
   //
   // If you have trouble getting all important capabilities together, check out the
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
