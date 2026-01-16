e732bf1e59cff76094394967122d35549bda02bc
John Jenkins <johnljenkins@hotmail.com>
2026-01-16 00:08:09 +0000

    chore(ci): run tests in parallel

--- example-project/next-15-react-19-app/src/app/input-shadow/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TestStage } from 'react-test-components';
-
-export default function InputShadow() {
-  return (
-    <div>
-      <TestStage name="input-shadow" />
-    </div>
-  );
-}
--- example-project/next-15-react-19-app/src/app/single-children-scoped/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TestStage } from 'react-test-components';
-
-export default function SingleChildrenScoped() {
-  return (
-    <div>
-      <TestStage name="single-children-scoped" />
-    </div>
-  );
-}
