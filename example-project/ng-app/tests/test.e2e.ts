50545e8beaf67de85dfceb0a1aa52b1074aeddb4
Jacob Bell <228905018+OS-jacobbell@users.noreply.github.com>
2026-05-27 11:48:23 -0600

    chore(angular): add test to call setFocus @method

--- example-project/ng-app/tests/test.e2e.ts
+++ example-project/ng-app/tests/test.e2e.ts
@@ -1,5 +1,6 @@
 /// <reference types="@wdio/globals/types" />
 /// <reference types="@wdio/mocha-framework" />
+import os from 'node:os';
 import { expect, $, browser } from '@wdio/globals';
 
 describe('Stencil Angular Integration', () => {
@@ -19,6 +20,19 @@ describe('Stencil Angular Integration', () => {
     await expect(counter.$('span')).toHaveText('2279');
     await minusButton.click();
     await expect(counter.$('span')).toHaveText('2278');
+  });
+
+  it('should focus the inner input when setFocus is called', async function () {
+    if (os.platform() === 'win32') {
+      return this.skip()
+    }
+    await browser.execute(async () => await customElements.whenDefined('my-checkbox'));
+    await browser.execute((el: any) => el.setFocus(), await $('my-checkbox'));
+    const isFocused = await browser.execute(() => {
+      const el = document.querySelector('my-checkbox') as any;
+      return el?.shadowRoot?.activeElement?.tagName.toLowerCase() === 'input';
+    });
+    expect(isFocused).toBe(true);
   });
 
   it('should transform tag names correctly', async () => {
