ae6c9452805775a4a90568fb63bcfb9e6aa6a204
Jacob Bell <228905018+OS-jacobbell@users.noreply.github.com>
2026-06-02 14:06:09 -0600

    chore(angular): fix my-range component

--- example-project/ng-app/src/app/component-tests/range-tests/range-tests.ts
+++ example-project/ng-app/src/app/component-tests/range-tests/range-tests.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MyRange, SelectValueAccessor } from 'component-library-angular';
+import { MyCounter, MyRange, SelectValueAccessor } from 'component-library-angular';
 
 @Component({
   selector: 'app-range-tests',
   standalone: true,
-  imports: [MyRange, SelectValueAccessor, FormsModule],
+  imports: [MyCounter, MyRange, SelectValueAccessor, FormsModule],
   templateUrl: './range-tests.html',
   styles: ``,
 })
 export class RangeTests {
-  testText: any;
+  rangeValue: number = 0;
+  min: number = 0;
+  max: number = 100;
 }
